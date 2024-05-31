06a34e14b78d2568fe85f51d6f00e1bd27b469f0
boburDev <negmatov1907@gmail.com>
2024-05-31 12:07:01 +0500

    Update example.sql

--- example.sql
+++ example.sql
@@ -3,7 +3,7 @@ drop database crm;
 CREATE DATABASE crm;
 \c crm
 
-ALTER USER user_name WITH PASSWORD 'bobur1907';
+ALTER USER postgres WITH PASSWORD 'bobur1907';
 
 drop table company_branches, costs, courses, employers, group_attendences,
 groups, rooms, student_cashes, student_payments, students cascade;
