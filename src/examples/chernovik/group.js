7bba2fe98b4b7515396bfdb66c01f551eb7d39da
boburDev <negmatov1907@gmail.com>
2024-08-12 14:59:37 +0500

    Update group.js

--- src/examples/chernovik/group.js
+++ src/examples/chernovik/group.js
@@ -5,3 +5,20 @@ data = await groupRepository.findOne({
     },
     relations: ['attendence', 'employer', 'room', 'course']
 })
+
+
+    (async () => {
+        let response = await fetch("http://api.uz-fox.uz/auth/login", {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify({
+                userphone: "998998616951",
+                password: "111111",
+                subdomain: "soffit"
+            }),
+        });
+        console.log(await response.json());
+    })();
