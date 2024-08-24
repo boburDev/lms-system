ebcb3affd546319771b61a677f3f692fbae007cc
boburDev <negmatov1907@gmail.com>
2024-08-24 17:56:47 +0500

    form started and language jsons added to projects

---
+++ src/modules/forms/schema.ts
@@ -0,0 +1,25 @@
+import { gql } from 'apollo-server-express'
+
+export default gql`
+type Query {
+  forms: [Form]!
+  formById(Id: String!): Form!
+}
+
+type Mutation {
+  addForm(input: AddFormInput) : Form!
+}
+
+type Form {
+  formId: String!
+  formTitle: String!
+}
+
+input AddFormInput {
+  formTitle: String!
+}
+input UpdateFormInput {
+  formId: String!
+  formTitle: String
+}
+`
